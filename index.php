135013c4288b0c38583baf54600e058a2108e2a8
liucongcong65 <15081773165@163.com>
2018-10-16 14:10:01 +0800

    添加index.php文件

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return $default;
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        return $value;
+    }
+}
+
+$env_file = parse_ini_file(__DIR__."/env");
+foreach ($env_file as $k=>$v) {
+    putenv($k.'='.$v);
+}
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/framework3/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',env('YII_DEBUG'));
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+require_once __DIR__.'/protected/vendor/autoload.php';
+Yii::createWebApplication($config)->run();
